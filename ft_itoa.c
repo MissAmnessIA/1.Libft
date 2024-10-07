caa20aa4c11475a4eaaa54e78bb0b2873040311e
Veronica <veronicajoanna30@gmail.com>
2024-10-08 00:59:21 +0200

    Uploaded

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,16 +6,18 @@
 /*   By: vmesa-ke <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 22:08:01 by vmesa-ke          #+#    #+#             */
-/*   Updated: 2024/10/07 22:08:05 by vmesa-ke         ###   ########.fr       */
+/*   Updated: 2024/10/08 00:58:19 by vmesa-ke         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
 
-int	get_int_len(int num, int isnegative)
+long	get_int_len(long num, int isnegative)
 {
-	int	len;
+	long	len;
 
 	len = 0;
+	if (num == 0)
+		return (1);
 	while (num != 0)
 	{
 		num /= 10;
@@ -26,11 +28,13 @@ int	get_int_len(int num, int isnegative)
 	return (len);
 }
 
-char	*convert(int i, int numtocovert, int isnegative)
+char	*convert(long i, long numtocovert, int isnegative)
 {
 	char	*converted;
 
-	converted = (char *)malloc((i + 1) * sizeof(char));
+	converted = (char *)malloc(i + 1);
+	if (!converted)
+		return(NULL);
 	converted[i] = '\0';
 	while (isnegative <= i)
 	{
@@ -45,20 +49,19 @@ char	*convert(int i, int numtocovert, int isnegative)
 
 char	*ft_itoa(int n)
 {
-	int		len_n;
-	char	*n_str;
-	int		neg;
+	long		len_n;
+	char		*n_str;
+	int			neg;
+	long		cpi;
 
+	cpi = (long)n;
 	neg = 0;
 	if (n < 0)
 	{
 		n *= -1;
 		neg = 1;
 	}
-	if (n == 0)
-		len_n = 1;
-	else
-		len_n = get_int_len(n, neg);
-	n_str = convert(len_n, n, neg);
+	len_n = get_int_len(cpi, neg);
+	n_str = convert(len_n, cpi, neg);
 	return (n_str);
 }
