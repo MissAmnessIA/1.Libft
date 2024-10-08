a407b98cdad2b282340d03e32a900f2106f84194
Veronica Joanna Mesa Kenig <vmesa-ke@c1r16s2.42madrid.com>
2024-10-08 18:52:40 +0200

    Uploaded

--- ft_itoa.c
+++ ft_itoa.c
@@ -10,14 +10,14 @@
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
+#include <stdio.h>
 
-long	get_int_len(long num, int isnegative)
+
+int	get_int_len(int num, int isnegative)
 {
-	long	len;
+	int	len;
 
 	len = 0;
-	if (num == 0)
-		return (1);
 	while (num != 0)
 	{
 		num /= 10;
@@ -28,19 +28,26 @@ long	get_int_len(long num, int isnegative)
 	return (len);
 }
 
-char	*convert(long i, long numtocovert, int isnegative)
+char	*convert(long i, int numtocovert, int isnegative)
 {
 	char	*converted;
 
-	converted = (char *)malloc(i + 1);
+	converted = (char *)malloc((i + 1) * sizeof(char));
 	if (!converted)
-		return(NULL);
-	converted[i] = '\0';
-	while (isnegative <= i)
+		return (NULL);
+	if (i == 0)
 	{
-		converted[i - 1] = ((numtocovert % 10) + 48);
-		numtocovert /= 10;
-		i--;
+		converted[0] = '0';
+		converted[1] = '\0';
+	}
+	else{
+		converted[i] = '\0';
+		while (isnegative <= i)
+		{
+			converted[i - 1] = ((numtocovert % 10) + 48);
+			numtocovert /= 10;
+			i--;
+		}
 	}
 	if (isnegative)
 		converted[0] = '-';
@@ -49,19 +56,45 @@ char	*convert(long i, long numtocovert, int isnegative)
 
 char	*ft_itoa(int n)
 {
-	long		len_n;
-	char		*n_str;
-	int			neg;
-	long		cpi;
+	int		len_n;
+	char	*n_str;
+	int		neg;
 
-	cpi = (long)n;
 	neg = 0;
-	if (n < 0)
-	{
-		n *= -1;
-		neg = 1;
+	if (n == 2147483647){
+		n_str = (char *)malloc(11);
+		if(n_str == NULL)
+			return (NULL);
+		n_str = "2147483647";
 	}
-	len_n = get_int_len(cpi, neg);
-	n_str = convert(len_n, cpi, neg);
+	else if (n == -2147483648){
+		n_str = (char *)malloc(12);
+		if (n_str == NULL)
+			return(NULL);
+		n_str = "-2147483648";
+	}			
+	else{		
+		if (n < 0)
+		{
+			n *= -1;
+			neg = 1;
+		}
+		if (n == 0)
+			len_n = 1;
+		else
+			len_n = get_int_len(n, neg);
+		n_str = convert(len_n, n, neg);
+	}
+	
 	return (n_str);
+}
+int main(void)
+{
+	long n = 0;
+	char *c = ft_itoa(n);
+
+
+	printf ("Mi lib: %s \n", c);
+	printf("Size: %zu", ft_strlen(c));
+	return(0);
 }
